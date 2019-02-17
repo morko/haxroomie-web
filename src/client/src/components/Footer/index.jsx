0c994c0578efb8ba51d13ea9e6178ed038ea4b9a
Oskari Pöntinen <mail.morko@gmail.com>
2019-02-17 12:00:59 +0200

    fixed footer element aligning

--- src/client/src/components/Footer/index.jsx
+++ src/client/src/components/Footer/index.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import './index.css';
 import GitHubLinkImg from './GitHub-Mark-Light-64px.png';
-import { Media, Row, Col } from 'reactstrap';
+import { Media, Row, Col, Container } from 'reactstrap';
 
 export default function Footer() {
   return (
-    <div className="Footer">
-      <Row>
-        <Col className="Footer-copyright" cs="6">
-          <a href="https://www.github.com/morko/haxroomie-web/blob/master/LICENSE">&copy; 2019 Oskari Pöntinen</a>
-        </Col>
-        <Col xs="6">
-          <Media>
-            <Media 
-            className="Footer-github" 
-            body 
-            href="https://www.github.com/morko/haxroomie-web">
-              GitHub
+    <footer className="Footer page-footer">
+      <Container>
+        <Row>
+          <Col className="Footer-copyright" xs="7">
+            <p><a href="https://www.github.com/morko/haxroomie-web/blob/master/LICENSE">&copy; 2019 Oskari Pöntinen</a></p>
+          </Col>
+          <Col xs="5">
+            <Media>
+              <Media 
+              className="Footer-github" 
+              body 
+              href="https://www.github.com/morko/haxroomie-web"
+              style={{color: "white"}}>
+                GitHub
+              </Media>
+              <Media right href="https://www.github.com/morko/haxroomie-web">
+                <Media object src={GitHubLinkImg} alt="Project in GitHub" />
+              </Media>
             </Media>
-            <Media right href="https://www.github.com/morko/haxroomie-web">
-              <Media object src={GitHubLinkImg} alt="Project in GitHub" />
-            </Media>
-          </Media>
-        </Col>
-      </Row>
-
-
-    </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 }
