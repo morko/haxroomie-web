b5bde8c250e1f2bb32c9b1db837eb098454d0fa3
Oskari Pöntinen <mail.morko@gmail.com>
2019-02-17 12:22:14 +0200

    do not use logger middleware if in production environment

--- src/client/src/store.js
+++ src/client/src/store.js
@@ -1,9 +1,15 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
 import reducer from './reducers';
 
-const middleware = applyMiddleware(thunk, logger);
+const inProduction = process.env.NOD_ENV !== 'development';
+
+let middleware;
+if (!inProduction) {
+  middleware = applyMiddleware(thunk, logger);
+} else {
+  middleware = applyMiddleware(thunk);
+}
 
 export default createStore(reducer, middleware);
