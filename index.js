3faf5555fbc666a72244448f86d5a3c8f6047189
Oskari Pöntinen <mail.morko@gmail.com>
2019-02-17 18:49:57 +0200

    implemented own command line arguments / config parsing
    - the default values were generating some weird results

--- index.js
+++ index.js
@@ -14,47 +14,56 @@ const argv = require(`yargs`)
 
   .alias(`p`, `port`)
   .describe(`p`, `Set a port that the app is listening to for HTTP connections.`)
-  .default(`port`, config.server.port)
 
   .alias(`b`, `haxroomie-port`)
-  .describe(`b`, `Set a local port that will be used to communicate with headless browser.`)
-  .default(`haxroomie-port`, config.haxroomie.port)
+  .describe(`haxroomie-port`, `Set a local port that will be used to communicate with headless browser.`)
 
   .alias(`w`, `window`)
-  .boolean(`window`)
   .describe(`window`, `Makes haxroomie run windowed (useful for debugging)`)
-  .default(`window`, config.haxroomie.window)
-
-  .alias(`s`, `secret`)
-  .describe(`s`, `Set a secret salt used to sign the session ID cookie.`)
-  .default(`secret`, config.server.secret)
 
   .alias(`e`, `secure`)
   .describe(`secure`, `Secure the sessions (requires HTTPS support)`)
-  .default(`secret`, config.server.secure)
 
-  .alias(`sq`, `sqlite-storage-path`)
-  .describe(`sq`, `Set a path from where sqlite database will used from.`)
-  .default(`sqlite-storage-path`, config.server.storage)
+  .alias(`s`, `secret`)
+  .describe(`secret`, `Set a secret salt used to sign the session ID cookie.`)
+
+  .alias(`o`, `storage`)
+  .describe(`storage`, `Set a path from where sqlite database will used from.`)
 
   .argv;
 
 (async function bootstrap() {
 
+  let cfg = parseConfig(config);
+
   const haxroomie = new Haxroomie({
-    headless: !argv.window,
+    headless: !cfg.window,
     port: argv.haxroomiePort
   });
   await haxroomie.createBrowser();
 
   const server = new HRWebServer({
     haxroomie: haxroomie,
-    port: argv.port,
-    secret: argv.secret,
-    secure: argv.secure,
-    storage: argv.storage,
-    defaultUser: config.server.user
+    port: cfg.port,
+    secret: cfg.secret,
+    secure: cfg.secure,
+    storage: cfg.storage,
+    defaultUser: cfg.defaultUser
   });
   await server.listen();
   logger.info(`Haxroomie web started.`);
 })();
+
+function parseConfig(config) {
+  let cfg = {
+    window: argv.hasOwnProperty('window') ? argv.window : config.haxroomie.window,
+    haxroomiePort: argv.haxroomiePort || config.haxroomie.port,
+    port: argv.port || config.server.port,
+    secret: argv.secret || config.server.secret,
+    secure: argv.hasOwnProperty('secure') ? argv.secure : config.server.secure,
+    storage: argv.storage || config.server.storage,
+    defaultUser: config.server.user
+  }
+
+  return cfg;
+}
