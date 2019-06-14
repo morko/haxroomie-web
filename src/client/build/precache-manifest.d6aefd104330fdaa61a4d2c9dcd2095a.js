0bbc200a7953be9b323d6c18f2d6666b5c658add
Oskari Pöntinen <mail.morko@gmail.com>
2019-06-14 09:33:53 +0300

    updated packages

---
+++ src/client/build/precache-manifest.d6aefd104330fdaa61a4d2c9dcd2095a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a402b4cc7248d88a01c8e24c2db09e6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5db737a35dc364ce175",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "5ca4913a2725df6691e1",
+    "url": "/static/css/main.9114f221.chunk.css"
+  },
+  {
+    "revision": "e5db737a35dc364ce175",
+    "url": "/static/js/2.bcdd787a.chunk.js"
+  },
+  {
+    "revision": "5ca4913a2725df6691e1",
+    "url": "/static/js/main.f94c9a69.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9fa2491ebb4852bb2c780c26360997fd",
+    "url": "/static/media/logo.9fa2491e.png"
+  },
+  {
+    "revision": "0afadfb3f421097550fb88683fffae10",
+    "url": "/static/media/stripes.0afadfb3.png"
+  }
+]);
